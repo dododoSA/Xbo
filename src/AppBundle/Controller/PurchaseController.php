9bff99557af1df295c1df982834b5c1e56d9cbb8
szk_akihiro <dododosa14@gmail.com>
2019-12-30 18:19:54 +0900

    first commit

---
+++ src/AppBundle/Controller/PurchaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class PurchaseController extends FOSRestController {
+    /**
+     * @Post(/)
+     */
+}
---
+++ src/AppBundle/Entity/Purchase.php
@@ -0,0 +1,55 @@
+<?php 
+
+namespace AppBundle\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="purchase")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PurchaseRepository")
+ */
+
+class Purchase {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $purchasedAt;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+
+    public function getPurchasedAt() {
+        return $this->purchasedAt;
+    }
+
+    public function setPurchasedAt(DateTime $purchasedAt) {
+        $this->purchasedAt = $purchasedAt;
+    }
+}
